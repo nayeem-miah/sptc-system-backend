ee05261b99a81cbb58979732808b4777458e2a9e
nayeem-miah <nayeem5113a@gmail.com>
2026-06-03 12:52:03 +0600

    task done

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -3,6 +3,8 @@ import { UserRoutes } from "../modules/user/user.route";
 import { AuthRoutes } from "../modules/auth/auth.route";
 import { ProjectRoutes } from "../modules/project/project.route";
 import { ActivityLogRoutes } from "../modules/activityLog/activityLog.route";
+import { TaskRoutes } from "../modules/task/task.route";
+import { DashboardRoutes } from "../modules/dashboard/dashboard.route";
 
 const router = Router();
 
@@ -10,9 +12,7 @@ router.use("/users", UserRoutes);
 router.use("/auth", AuthRoutes);
 router.use("/projects", ProjectRoutes);
 router.use("/activities", ActivityLogRoutes);
-
-
-
-
+router.use("/tasks", TaskRoutes);
+router.use("/dashboard", DashboardRoutes);
 
 export default router;
